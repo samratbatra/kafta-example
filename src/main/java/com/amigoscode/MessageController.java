8887aa0525bc09c7e107cc78255952191acbf67e
samratbatra <samratbatra8@gmail.com>
2023-08-22 02:25:13 +0530

    Kafka String message

---
+++ src/main/java/com/amigoscode/MessageController.java
@@ -0,0 +1,20 @@
+package com.amigoscode;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/messages")
+public class MessageController {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping
+    public void publish(@RequestBody MessageRequest messageRequest){
+        kafkaTemplate.send("amigoscode", messageRequest.message());
+
+    }
+}
